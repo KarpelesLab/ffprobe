c298ab5edc4fd5635c7c6e88a2c6f6f9f6016a7b
Mark Karpeles <magicaltux@gmail.com>
2023-11-04 10:19:26 +0900

    add Fraction.Frac() to retrieve the actual fraction values

--- fraction.go
+++ fraction.go
@@ -10,26 +10,34 @@ type Fraction string // typically: 25/1, 30000/1001 or 1/90000 etc
 
 // Value returns the float value for the given framerate, or math.NaN() if the value could not be parsed
 func (f Fraction) Value() float64 {
+	a, b := f.Frac()
+	if b == 0 {
+		return math.NaN()
+	}
+	return float64(a) / float64(b)
+}
+
+func (f Fraction) Frac() (int, int) {
 	p := strings.IndexByte(string(f), '/')
 	if p == -1 {
-		// parse as float
-		v, err := strconv.ParseFloat(string(f), 64)
+		// parse as int
+		v, err := strconv.ParseInt(string(f), 10, 64)
 		if err != nil {
-			return math.NaN()
+			return 0, 0
 		}
-		return v
+		return int(v), 1
 	}
 	a := string(f[:p])   // 25
 	b := string(f[p+1:]) // 1
 
-	ai, err := strconv.ParseUint(a, 10, 64)
+	ai, err := strconv.ParseInt(a, 10, 64)
 	if err != nil {
-		return math.NaN()
+		return 0, 0
 	}
 	bi, err := strconv.ParseUint(b, 10, 64)
 	if err != nil {
-		return math.NaN()
+		return 0, 0
 	}
 
-	return float64(ai) / float64(bi)
+	return int(ai), int(bi)
 }
